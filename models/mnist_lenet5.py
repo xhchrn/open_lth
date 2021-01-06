e9e8d3ade2507b5eb3d28c3521633fc69ab064bc
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-01-06 11:38:14 -0600

    Change default settings

--- models/mnist_lenet5.py
+++ models/mnist_lenet5.py
@@ -95,9 +95,9 @@ class Model(base.Model):
         )
 
         training_hparams = hparams.TrainingHparams(
-            optimizer_name='sgd',
-            lr=0.1,
-            training_steps='40ep',
+            optimizer_name='adam',
+            lr=0.001,
+            training_steps='20ep',
         )
 
         pruning_hparams = sparse_global.PruningHparams(
