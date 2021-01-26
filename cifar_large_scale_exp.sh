48ad467e10d09851e0cbb43b27e601201557fca9
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-01-25 23:04:37 -0600

    CIFAR_exp first part

--- cifar_large_scale_exp.sh
+++ cifar_large_scale_exp.sh
@@ -1,7 +1,7 @@
 # export replicate=1
 
 # 14: lottery_47a5d7b2f955ca86f12cc1e70e1ce588
-# 20: lottery_231c88c353aecc912eada550c58eff10 
+# 20: lottery_231c88c353aecc912eada550c58eff10
 # 32: lottery_d1bce129beebea6ae76df1c61e53d1e6
 # 44: lottery_9177ecc955f02f6db621dca334ce6f72
 # 56: lottery_77378b9059907f2145d5a60a20b91043
@@ -16,9 +16,11 @@
 export source_model="cifar_resnet_14"
 export target_model="cifar_resnet_20"
 export mapping_inter="0:0;1:1,2"
-export mapping_head="0:0;1:1,2"
-export mapping_tail="0:0;1:1,2"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_head="0:0;1:1,2"
+# export mapping_tail="0:0;1:1,2"
+# export mapping_first="0:0;1:1,2"
+# export mapping_last="0:0;1:1,2"
+for mapping in $mapping_inter; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -26,9 +28,11 @@ done
 export source_model="cifar_resnet_14"
 export target_model="cifar_resnet_32"
 export mapping_inter="0:0;1:1,2,3,4"
-export mapping_head="0:0;1:1,2,3,4"
-export mapping_tail="0:0;1:1,2,3,4"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_head="0:0;1:1,2,3,4"
+# export mapping_tail="0:0;1:1,2,3,4"
+# export mapping_first="0:0;1:1,2,3,4"
+# export mapping_last="0:0;1:1,2,3,4"
+for mapping in $mapping_inter; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -36,9 +40,11 @@ done
 export source_model="cifar_resnet_14"
 export target_model="cifar_resnet_44"
 export mapping_inter="0:0;1:1,2,3,4,5,6"
-export mapping_head="0:0;1:1,2,3,4,5,6"
-export mapping_tail="0:0;1:1,2,3,4,5,6"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_head="0:0;1:1,2,3,4,5,6"
+# export mapping_tail="0:0;1:1,2,3,4,5,6"
+# export mapping_first="0:0;1:1,2,3,4,5,6"
+# export mapping_last="0:0;1:1,2,3,4,5,6"
+for mapping in $mapping_inter; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -46,9 +52,11 @@ done
 export source_model="cifar_resnet_14"
 export target_model="cifar_resnet_56"
 export mapping_inter="0:0;1:1,2,3,4,5,6,7,8"
-export mapping_head="0:0;1:1,2,3,4,5,6,7,8"
-export mapping_tail="0:0;1:1,2,3,4,5,6,7,8"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_head="0:0;1:1,2,3,4,5,6,7,8"
+# export mapping_tail="0:0;1:1,2,3,4,5,6,7,8"
+# export mapping_first="0:0;1:1,2,3,4,5,6,7,8"
+# export mapping_last="0:0;1:1,2,3,4,5,6,7,8"
+for mapping in $mapping_inter; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -56,10 +64,12 @@ done
 # 20 to 14
 export source_model="cifar_resnet_20"
 export target_model="cifar_resnet_14"
-export mapping_inter="0:0;1:1"
 export mapping_head="0:0;2:1"
 export mapping_tail="0:0;1:1"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_tail="0:0;1:1"
+# export mapping_first="0:0;2:1"
+# export mapping_last="0:0;1:1"
+for mapping in $mapping_head $mapping_tail; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -67,29 +77,38 @@ done
 export source_model="cifar_resnet_20"
 export target_model="cifar_resnet_32"
 export mapping_inter="0:0;1:1,2;2:3,4"
-export mapping_head="0:0;1:1,3;2:2,4"
-export mapping_tail="0:0;1:1,3;2:2,4"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+export mapping_head1="0:0;1:1,3;2:2,4"
+export mapping_head2="0:0;1:1,2,3;2:4"
+export mapping_tail1="0:0;1:1;2:2,3,4"
+for mapping in $mapping_inter $mapping_head1 $mapping_head2 $mapping_tail1; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
 # 20 to 44
 export source_model="cifar_resnet_20"
 export target_model="cifar_resnet_44"
-export mapping_inter="0:0;1:1,2,3;2:4,5,6"
-export mapping_head="0:0;1:1,3,5;2:2,4,6"
-export mapping_tail="0:0;1:1,3,5;2:2,4,6"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+export mapping_inter3="0:0;1:1,2,3;2:4,5,6"
+export mapping_inter2_head2="0:0;1:1,2,3,4;2:5,6"
+export mapping_inter2_tail2="0:0;1:1,2;2:3,4,5,6"
+export mapping_head1="0:0;1:1,2,3,4,5;2:6"
+export mapping_head2="0:0;1:1,3,5;2:2,4,6"
+export mapping_tail1="0:0;1:1;2:2,3,4,5,6"
+for mapping in $mapping_inter3 $mapping_inter2_head2 $mapping_inter2_tail2 $mapping_head1 $mapping_head2 $mapping_tail1; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
 # 20 to 56
 export source_model="cifar_resnet_20"
 export target_model="cifar_resnet_56"
-export mapping_inter="0:0;1:1,2,3,4;2:5,6,7,8"
-export mapping_head="0:0;1:1,3,5,7;2:2,4,6,8"
-export mapping_tail="0:0;1:1,3,5,7;2:2,4,6,8"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+export mapping_inter4="0:0;1:1,2,3,4;2:5,6,7,8"
+export mapping_inter3_head2="0:0;1:1,2,3,4,5;2:6,7,8"
+export mapping_inter3_tail2="0:0;1:1,2,3;2:4,5,6,7,8"
+export mapping_inter2_head4="0:0;1:1,2,5,6;2:3,4,7,8"
+export mapping_inter2_tail2="0:0;1:1,2;2:3,4,5,6,7,8"
+export mapping_head1="0:0;1:1,2,3,4,5,6,7;2:8"
+export mapping_head2="0:0;1:1,3,5,7;2:2,4,6,8"
+export mapping_tail1="0:0;1:1;2:2,3,4,5,6,7,8"
+for mapping in $mapping_inter4 $mapping_inter3_head2 $mapping_inter3_tail2 $mapping_inter2_head2 $mapping_inter2_head4 $mapping_inter2_tail2 $mapping_head1 $mapping_head2 $mapping_tail1; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -175,7 +194,7 @@ done
 #     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
 #         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 # done
-# 
+#
 # # 56 to 14
 # export source_model="cifar_resnet_56"
 # export target_model="cifar_resnet_14"
