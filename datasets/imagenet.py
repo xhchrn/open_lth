46c48a2fafdfd137309418c5b0b5dd96cc7d6ab6
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-01-27 23:02:24 -0600

    ImageNet exp fixed bug

--- datasets/imagenet.py
+++ datasets/imagenet.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import concurrent
+import concurrent.futures
 import numpy as np
 import os
 from PIL import Image
