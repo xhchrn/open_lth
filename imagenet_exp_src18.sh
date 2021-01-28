3ca7f05c25908cd17654f7cb4da5704e740ba55e
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-01-28 00:44:33 -0600

    ResNet-18 on ImageNet branch experiments

---
+++ imagenet_exp_src18.sh
@@ -0,0 +1,15 @@
+export source_model="imagenet_resnet_18"
+export target_model="imagenet_resnet_26"
+export mapping1="0:0;1:1|0:0;1:1,2|0:0;1:1,2,3|0:0;1:1,2"
+for mapping in $mapping1; do
+    python open_lth.py lottery_branch change_depth --num_workers 16 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_18 --levels=6 --rewinding_steps 5ep --replicate=$replicate \
+        --target_model_name $target_model --block_mapping "${mapping}" --levels=6
+done
+
+export source_model="imagenet_resnet_18"
+export target_model="imagenet_resnet_34"
+export mapping="0:0;1:1,2|0:0;1:1,2,3|0:0;1:1,2,3,4,5|0:0;1:1,2"
+for mapping in $mapping1; do
+    python open_lth.py lottery_branch change_depth --num_workers 16 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_18 --levels=6 --rewinding_steps 5ep --replicate=$replicate \
+        --target_model_name $target_model --block_mapping "${mapping}" --levels=6
+done
