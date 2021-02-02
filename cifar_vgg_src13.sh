aa8d69e100b2f105dbd9adb3c61003fe9ad8c15e
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-02-02 01:32:42 -0600

    Source VGG-13 exp

---
+++ cifar_vgg_src13.sh
@@ -0,0 +1,17 @@
+export source_model="cifar_vgg_13"
+export target_model="cifar_vgg_16"
+export mapping_append="0:0;1:1|0:0;1:1|0:0,1;1:2|0:0,1;1:2|0:0,1;1:2"
+for mapping in $mapping_append; do
+    python open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_vgg_16 --model_name $source_model --rewinding_steps 200it --replicate=$replicate \
+        --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
+done
+
+export source_model="cifar_vgg_13"
+export target_model="cifar_vgg_19"
+export mapping_append="0:0;1:1|0:0;1:1|0:0,2;1:1,3|0:0,2;1:1,3|0:0,2;1:1,3"
+export mapping_inter="0:0;1:1|0:0;1:1|0:0,1;1:2,3|0:0,1;1:2,3|0:0,1;1:2,3"
+for mapping in $mapping_append $mapping_inter; do
+    python open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_vgg_16 --model_name $source_model --rewinding_steps 200it --replicate=$replicate \
+        --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
+done
+
