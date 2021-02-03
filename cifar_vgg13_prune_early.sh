87dd566a400bddd024df58f812d721c30572f895
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-02-03 12:09:48 -0600

    CIFAR VGG Early Prune Exp

---
+++ cifar_vgg13_prune_early.sh
@@ -0,0 +1,2 @@
+python open_lth.py lottery_branch prune_early --num_workers 4 --default_hparams=cifar_vgg_16 --model_name cifar_vgg_13 --rewinding_steps 200it --levels=0-16 --strategy sparse_global --seed 7 --replicate=$replicate
+python open_lth.py lottery_branch prune_early --num_workers 4 --default_hparams=cifar_vgg_16 --model_name cifar_vgg_13 --rewinding_steps 200it --levels=0-16 --strategy snip_global --seed 7 --replicate=$replicate 
