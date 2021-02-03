20c4f1ab6fff3458cc81216e40216a9176a8c3ca
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-02-03 07:35:36 -0600

    Reduce some exp

--- cifar_large_scale_exp_src44.sh
+++ cifar_large_scale_exp_src44.sh
@@ -22,9 +22,9 @@ done
 export source_model="cifar_resnet_44"
 export target_model="cifar_resnet_32"
 export mapping_inter="0:0;1:1;2:2;3:3;5:4"
-export mapping_head="0:0;3:1;4:2;5:3;6:4"
+# export mapping_head="0:0;3:1;4:2;5:3;6:4"
 export mapping_tail="0:0;1:1;2:2;3:3;4:4"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+for mapping in $mapping_inter $mapping_tail; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
@@ -33,8 +33,8 @@ export source_model="cifar_resnet_44"
 export target_model="cifar_resnet_56"
 export mapping_inter="0:0;1:1,2;2:3,4;3:5;4:6;5:7;6:8"
 export mapping_head="0:0;1:1,3;2:2,4;3:5;4:6;5:7;6:8"
-export mapping_tail="0:0;1:1;2:2;3:3;4:4;5:5,7;6:6,8"
-for mapping in $mapping_inter $mapping_head $mapping_tail; do
+# export mapping_tail="0:0;1:1;2:2;3:3;4:4;5:5,7;6:6,8"
+for mapping in $mapping_inter $mapping_head; do
     python3 open_lth.py lottery_branch change_depth --num_workers 4 --default_hparams=cifar_resnet_20 --model_name $source_model --rewinding_steps 1000it --replicate=$replicate \
         --target_model_name $target_model --block_mapping "${mapping}" --levels=0-16
 done
