b19b259522cd22af1abfa3370e5a7c4dcbb7165a
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-03-27 23:42:50 -0500

    Fixed bug

--- lottery/branch/change_depth_linear_connect.py
+++ lottery/branch/change_depth_linear_connect.py
@@ -3,7 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import os
 import copy
+import numpy as np
 from lottery.branch import base
 import models.registry
 from pruning.mask import Mask
