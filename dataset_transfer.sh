6b3d04a12a2f868ce851bd09b330ea57957c1de6
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-06-09 06:51:03 -0700

    Dataset transfer

--- dataset_transfer.sh
+++ dataset_transfer.sh
@@ -91,6 +91,37 @@ export target_model="imagenet_resnet_18"
 export source_dataset="imagenet"
 export target_dataset="cifar10_im"
 export mapping="0:0;1:1|0:0;1:1|0:0;1:1|0:0;1:1"
-python3 open_lth.py lottery_branch --num_workers 4 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_18 \
+python3 open_lth.py lottery_branch change_depth_dataset --num_workers 4 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_18 \
+    --dataset_name        $source_dataset --model_name        $source_model --rewinding_steps 5ep --replicate=$replicate \
+    --target_dataset_name $target_dataset --target_model_name $target_model --block_mapping "${mapping}" --levels=6
+
+
+# From imagenet_resnet26 -> cifar10_im_resnet26
+export source_model="imagenet_resnet_26"
+export target_model="imagenet_resnet_26"
+export source_dataset="imagenet"
+export target_dataset="cifar10_im"
+export mapping="0:0;1:1|0:0;1:1;2:2|0:0;1:1;2:2;3:3|0:0;1:1;2:2"
+python3 open_lth.py lottery_branch change_depth_dataset --num_workers 4 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_26 \
+    --dataset_name        $source_dataset --model_name        $source_model --rewinding_steps 5ep --replicate=$replicate \
+    --target_dataset_name $target_dataset --target_model_name $target_model --block_mapping "${mapping}" --levels=6
+
+# From imagenet_resnet26 -> cifar10_im_resnet18
+export source_model="imagenet_resnet_26"
+export target_model="imagenet_resnet_18"
+export source_dataset="imagenet"
+export target_dataset="cifar10_im"
+export mapping="0:0;1:1|0:0;1:1|0:0;1:1|0:0;1:1"
+python3 open_lth.py lottery_branch change_depth_dataset --num_workers 4 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_26 \
+    --dataset_name        $source_dataset --model_name        $source_model --rewinding_steps 5ep --replicate=$replicate \
+    --target_dataset_name $target_dataset --target_model_name $target_model --block_mapping "${mapping}" --levels=6
+
+# From imagenet_resnet34 -> cifar10_im_resnet34
+export source_model="imagenet_resnet_34"
+export target_model="imagenet_resnet_34"
+export source_dataset="imagenet"
+export target_dataset="cifar10_im"
+export mapping="0:0;1:1;2:2|0:0;1:1;2:2;3:3|0:0;1:1;2:2;3:3;4:4;5:5|0:0;1:1;2:2"
+python3 open_lth.py lottery_branch change_depth_dataset --num_workers 4 --default_hparams=imagenet_resnet_50 --model_name imagenet_resnet_34 \
     --dataset_name        $source_dataset --model_name        $source_model --rewinding_steps 5ep --replicate=$replicate \
     --target_dataset_name $target_dataset --target_model_name $target_model --block_mapping "${mapping}" --levels=6
